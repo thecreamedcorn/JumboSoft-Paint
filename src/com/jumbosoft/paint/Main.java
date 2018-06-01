98c6a4f8f9ae928d7c5baf1fdd64499134414449
nathanhleung <nathanhleung@users.noreply.github.com>
2018-06-01 15:50:19 -0400

    feat: open directly to new project instead of intropane

--- src/com/jumbosoft/paint/Main.java
+++ src/com/jumbosoft/paint/Main.java
@@ -7,6 +7,7 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import com.jumbosoft.paint.view.WorkPane;
 import com.jumbosoft.paint.view.IntroPane;
+import com.jumbosoft.paint.model.PaintCanvas;
 
 public class Main extends Application {
     private Stage stage;
@@ -15,7 +16,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         this.stage = primaryStage;
-        workPane = new WorkPane(IntroPane.introPane(), primaryStage);
+        // workPane = new WorkPane(IntroPane.introPane(), primaryStage);
+        workPane = new WorkPane(new PaintCanvas(500, 500), primaryStage);
         Scene scene = new Scene(workPane, 600, 600);
         primaryStage.setScene(scene);
         primaryStage.setTitle("JumboSoft Paint");
